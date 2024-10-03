c30f6e7980024a07619a8bc9e504b7940290b51c
VictoriaSCorreia <victoriacorreiavic@gmail.com>
2024-10-03 15:49:41 -0300

    update

--- MALocacao/src/main/java/com/project/MALocacao/MaLocacaoApplication.java
+++ MALocacao/src/main/java/com/project/MALocacao/MaLocacaoApplication.java
@@ -2,12 +2,19 @@ package com.project.MALocacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MaLocacaoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaLocacaoApplication.class, args);
 	}
 
+	@GetMapping("/index")
+	public String index(){
+		return "Olá Mundo!";
+	}
 }
